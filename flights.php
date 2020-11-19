7afeaaf622be495c5d61ee9af6da3716858b4a76
AlexLander123 <13492360+AlexLander123@users.noreply.github.com>
2020-11-19 17:54:16 +0800

    Add header template

---
+++ flights.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" style="height: 100%;">
+
+<head>
+    <?php require('head.php') ?>
+    <title>Flights</title>
+</head>
+
+<body class="d-flex flex-column h-100">
+    <?php require("header.php") ?>
+
+
+
+    <?php require('scripts.php') ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,5 @@
+<div class="d-flex justify-content-between">
+    <img src="images/airasia-logo.svg" alt="AirAsia Logo" height="100px">
+    <input class="btn btn-outline-primary btn-lg text-right align-self-center mr-5" type="button" value="Login">
+</div>
+<hr class="w-100">
